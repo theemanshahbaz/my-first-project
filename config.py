95a1f8a6a6c723562aebb661e4b4579aaa07e673
theemanshahbaz <emanshahbazemanshahbaz4@gmail.com>
2026-04-23 09:53:42 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+LIBRARY_NAME = "NextGen Library"
+FONT_HEADER = ("Helvetica", 24, "bold")
+FONT_NORMAL = ("Helvetica", 14)
+FONT_SMALL = ("Helvetica", 12)
+
+# Colors
+BG_MAIN = "#0f2027"
+BG_TAB = "#203a43"
+HEADER_GRADIENT_START = "#00f260"
+HEADER_GRADIENT_END = "#0575e6"
+
+BTN_PRIMARY = "#00d2ff"
+BTN_PRIMARY_HOVER = "#3a7bd5"
+BTN_SECONDARY = "#ff6ec7"
+BTN_SECONDARY_HOVER = "#d81b60"
+BTN_CANCEL = "#ff4d6d"
+BTN_CANCEL_HOVER = "#c70039"
+
+TREE_AVAILABLE = "#7bed9f"
+TREE_ISSUED = "#fffa65"
+TREE_OVERDUE = "#ff6b6b"
+TREE_HOVER = "#12c2e9"
+
+TEXT_LIGHT = "#ffffff"
+TEXT_HIGHLIGHT = "#ffdd59"
+TEXT_ALERT = "#ff4757"
